b0eb17664ecd7e74ce4eb1f329036cf0367bcc3a
Elizabeth Dinella <t-eldine@microsoft.com>
2016-07-14 14:13:20 -0700

    next and back buttons

--- src/alum-donation-ctrl.js
+++ src/alum-donation-ctrl.js
@@ -1,7 +1,7 @@
 angular.module('AlumDonation')
     .controller('AlumDonationCtrl', ['$scope', '$log', 'AlumDonationSvc', 'stripe', function ($scope, $log, alumDonationService, stripe) {
     
-        $scope.pageStep = "stepAddress";
+        $scope.pageStep = "stepAmount";
         $scope.cardNumber;
         $scope.expirationMonth;
         $scope.expirationYear;
@@ -11,6 +11,30 @@ angular.module('AlumDonation')
         stripeResponseHandler = function(status, response){
             $log.log(response);
         };
+        
+        $scope.next = function(){
+            switch($scope.pageStep){
+                case "stepAmount":
+                    $scope.pageStep = "stepCard";
+                    break;
+                case "stepCard":
+                    $scope.pageStep = "stepAddress";
+                    break;
+                case "stepAddress":
+                    $scope.submit();
+            }
+        }
+        
+        $scope.back = function(){
+            switch($scope.pageStep){
+                case "stepCard":
+                    $scope.pageStep = "stepAmount";
+                    break;
+                case "stepAddress":
+                    $scope.pageStep = "stepCard";
+            }
+        }
+        
         
         $scope.submit = function(){
             $log.log("submitting");
