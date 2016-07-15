2b016ea97164e73ab9a8ff271d3652e48cfc3eb8
elizabethdinella <elizabeth.a.dinella@gmail.com>
2016-07-14 21:09:12 -0400

    form validation

--- src/alum-donation-ctrl.js
+++ src/alum-donation-ctrl.js
@@ -2,17 +2,64 @@ angular.module('AlumDonation')
     .controller('AlumDonationCtrl', ['$scope', '$log', 'AlumDonationSvc', 'stripe', function ($scope, $log, alumDonationService, stripe) {
     
         $scope.pageStep = "stepAmount";
+        
+        //amountStep
+        $scope.amount;
+        $scope.amountValid;
+        
+        //cardStep
         $scope.cardNumber;
-        $scope.expirationMonth;
-        $scope.expirationYear;
+        $scope.cardValid;
+        $scope.expirationDate;
+        $scope.expValid;
         $scope.CVC;
+        $scope.cvcValid;
+        
+        //addressStep 
+        $scope.name;
+        $scope.nameValid;
+        $scope.addressOne;
+        $scope.addressValid;
+        $scope.addressTwo;
+        $scope.city;
+        $scope.cityValid;
+        $scope.state;
+        $scope.stateValid;
         $scope.states = usStates;
+        $scope.zipcode;
+        $scope.zipValid;
         
         stripeResponseHandler = function(status, response){
             $log.log(response);
         };
         
+        
+        $scope.validate = function(){
+            switch($scope.pageStep){
+                case "stepAmount":
+                    $scope.amountValid = getValidateClass($scope.amount && $scope.amount>0);
+                    return $scope.amountValid == true;
+                case "stepCard":
+                    $scope.cardValid = 
+                    getValidateClass(Stripe.card.validateCardNumber($scope.cardNumber));
+                    $scope.expValid = getValidateClass($scope.expirationDate && Stripe.card.validateExpiry($scope.expirationDate.getMonth()+1, $scope.expirationDate.getFullYear()));
+                    $scope.cvcValid = getValidateClass(Stripe.card.validateCVC($scope.CVC));
+                    return $scope.cardValid == true && $scope.expValid == true && $scope.cvcValid == true;
+                case "stepAddress":
+                    $scope.nameValid = getValidateClass($scope.name);
+                    $scope.addressValid = getValidateClass($scope.address);
+                    $scope.cityValid = getValidateClass($scope.city);
+                    $scope.stateValid = getValidateClass($scope.state);
+                    $scope.zipValid = getValidateClass($scope.zipcode && $scope.zipcode.length == 5);
+                    return $scope.nameValid == true && $scope.addressValid == true && $scope.cityValid == true && $scope.stateValid == true && $scope.zipValid == true;
+            }
+        }
+        
         $scope.next = function(){
+            if(!$scope.validate()){
+                return; 
+            }
+            
             switch($scope.pageStep){
                 case "stepAmount":
                     $scope.pageStep = "stepCard";
@@ -41,8 +88,8 @@ angular.module('AlumDonation')
             Stripe.card.createToken ({
                 number: $scope.cardNumber,
                 cvc: $scope.cvc,
-                exp_month: $scope.expirationMonth,
-                exp_year: $scope.expirationYear 
+                exp_month: $scope.expirationDate.getMonth()+1,
+                exp_year: $scope.expirationDate.getFullYear()
             }, stripeResponseHandler);
         };
 
---
+++ src/alum-donation-helpers.js
@@ -0,0 +1,7 @@
+getValidateClass = function(expression){
+        if(expression){
+            return true;
+        }else{
+            return "has-danger";
+        }
+}
